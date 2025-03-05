587641ecc9c521a5afa938b4fa0df7222ee38c4c
Ahmad <ahmad.alsayed@dci-student.org>
2025-03-05 11:48:29 +0100

    done

---
+++ data.js
@@ -0,0 +1,56 @@
+export const products = [
+    {
+      id: 1,
+      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      price: 109.95,
+      description:
+        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      rating: {
+        rate: 3.9,
+        count: 120,
+      },
+    },
+    {
+      id: 2,
+      title: "Mens Casual Premium Slim Fit T-Shirts ",
+      price: 22.3,
+      description:
+        "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+      category: "men's clothing",
+      image:
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      rating: {
+        rate: 4.1,
+        count: 259,
+      },
+    },
+    {
+      id: 3,
+      title: "Mens Cotton Jacket",
+      price: 55.99,
+      description:
+        "Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling, or other outdoors. Good gift choice for you or your family member. A warm-hearted love to Father, husband, or son in this Thanksgiving or Christmas Day.",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+      rating: {
+        rate: 4.7,
+        count: 500,
+      },
+    },
+    {
+      id: 10,
+      title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+      price: 109,
+      description:
+        "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5\" hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads. The perfect balance of performance and reliability. Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS, and application.)",
+      category: "electronics",
+      image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+      rating: {
+        rate: 2.9,
+        count: 470,
+      },
+    }
+  ];
+  
